348ea8419f23fba4d4341f41dbcd5ab0638b479e
Hemant Kumar <hkcc375@gmail.com>
2023-09-10 20:46:47 +0530

    Change to Program 24

--- HandsOn - Part One/25.c
+++ HandsOn - Part One/25.c
@@ -10,59 +10,64 @@ Date : 3rd September,2023
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main( int argc, char* argv[] )
+int main()
 {
-	pid_t pid_childOne = fork();
-	pid_t pid_childTwo = fork();
-	int status;
-	if( pid_childOne != 0 && pid_childTwo != 0 )
-	{
-		printf( "Hi, I am the parent of all the child processes, and "
-		        "my PID is %d. \n",
-		        getpid() );
-		printf( "I am now going to wait for the Child Process 1, and "
-		        "its PID is %d. \n",
-		        pid_childOne );
+	pid_t childAPID, childBPID, childCPID, terminatedChildPID;
+	int waitStatus;
 
-		while( !waitpid( pid_childOne, &status, 0 ) )
-			;
-
-		printf( "The Child Process 1 has successully terminated and "
-		        "now I am resuming execution. \n" );
-		printf( "The exit status code of Child Process 1 is %d. \n",
-		        WEXITSTATUS( status ) );
-	}
-	else if( pid_childOne == 0 && pid_childTwo != 0 )
+	if( ( childAPID = fork() ) == 0 )
 	{
-		printf( "Hi, I am Child Process 1 (in the order of creation), "
-		        "and my PID is %d. \n",
-		        getpid() );
-		printf( "Also, I have created Child Process 3 and its PID is "
-		        "%d. \n",
-		        pid_childTwo );
-		printf( "I am now going to sleep. \n" );
-		sleep( 20 );
-		printf( "I am now going to exit with status code 0. \n" );
+		printf( "Child A created...\n" );
+		printf( "Putting child A is now going to sleep.\n" );
+		sleep( 5 );
+		printf( "Child A is now awake!\n" );
 		exit( 0 );
-	}
-	else if( pid_childOne != 0 && pid_childTwo == 0 )
-	{
-		printf( "Hi, I am Child Process 2 (in the order of creation), "
-		        "and my PID is %d. \n",
-		        getpid() );
-		printf( "I am now going to exit with status code 1. \n" );
-		exit( 1 );
 	}
 	else
 	{
-		printf( "Hi, I am Child Process 3 (in the order of creation), "
-		        "and my PID is %d. \n",
-		        getpid() );
-		printf( "I am now going to exit with status code 2. \n" );
-		exit( 2 );
+		if( ( childBPID = fork() ) == 0 )
+		{
+			// Child B will enter this branch
+			printf( "Child B created...\n" );
+			printf( "Putting child B is now going to sleep.\n" );
+			sleep( 10 );
+			printf( "Child B is now awake!\n" );
+			exit( 0 );
+		}
+		else
+		{
+
+			if( ( childCPID = fork() ) == 0 )
+			{
+				printf( "Child C created...\n" );
+				printf( "Putting child C is now going to "
+				        "sleep.\n" );
+				sleep( 15 );
+				printf( "Child C is now awake!\n" );
+				exit( 0 );
+			}
+			else
+			{
+				terminatedChildPID =
+				    waitpid( childCPID, &waitStatus, 0 );
+
+				if( terminatedChildPID == childCPID )
+				{
+					if( waitStatus == 0 )
+						printf(
+						    "Child C has terminated "
+						    "with exit status = 0\n" );
+					else
+						printf( "Child has exited with "
+						        "some other "
+						        "exit status : %d\n",
+						        waitStatus );
+				}
+			}
+		}
 	}
-	return 0;
 }
