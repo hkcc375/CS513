45fcb64a2ad8d163f79771fe07b8d0f226212c67
Hemant Kumar <hkcc375@gmail.com>
2023-10-03 22:02:14 +0530

    Changed the structure of Program 14

--- HandsOn - Part Two/14.c
+++ HandsOn - Part Two/14.c
@@ -11,20 +11,14 @@ int main( int argc, char const *argv[] )
 	if( !( i = pipe( fd ) ) )
 	{
 		printf( "The pipe was created successfully. \n" );
-		sprintf( buffer, "This is just a test message if both the ends "
-		                 "of the pipe work correctly.\n" );
+		sprintf( buffer, "Test Message - Works Correctly.\n" );
 		int j = write( fd[1], buffer, sizeof( buffer ) );
-		if( j < 0 )
-			perror(
-			    "There was an error while performing write.\n" );
-		else
-		{
-			int k = read( fd[0], buffer, sizeof( buffer ) );
-			assert( k >= 0 );
-			printf( "Message from the read-end of the pipe "
-			        ": %s",
-			        buffer );
-		}
+		assert( j >= 0 );
+		int k = read( fd[0], buffer, sizeof( buffer ) );
+		assert( k >= 0 );
+		printf( "Message from the read-end of the pipe "
+		        ": %s",
+		        buffer );
 	}
 	else { perror( "There was an error while creating the pipe. \n" ); }
 	return 0;
