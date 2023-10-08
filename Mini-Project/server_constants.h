a79325f84caa8ac67b7835837a1a2a2bd091ee36
Hemant Kumar <hkcc375@gmail.com>
2023-10-08 22:19:07 +0530

    Added Server Constants

---
+++ Mini-Project/server_constants.h
@@ -0,0 +1,25 @@
+#ifndef SERVER_CONSTANTS
+#define SERVER_CONSTANTS
+
+#define INITIAL_PROMPT                                                         \
+	".................... Welcome Back to Academia Portal :: Course "      \
+	"Registration ....................\nLogin Type : Enter your choice { " \
+	"1. Admin 2.Faculty 3.Student }"
+
+#define ADMIN_MENU                                                             \
+	".......... Welcome to Admin Menu ..........\n1. Add Student\n2. "     \
+	"View Student Details\n3. Add Faculty\n4. View Faculty Details\n5. "   \
+	"Activate Student\n6. Block Student\n7. Modify Student Details\n8. "   \
+	"Modify Faculty Details\n9. Logout and Exit"
+
+#define FACULTY_MENU                                                           \
+	".......... Welcome to Faculty Menu ..........\n1. View Offering "     \
+	"Courses\n2. Add New Course\n3. Remove Course\n4. Update Course "      \
+	"Details\n5. Change Password\n6. Logout and Exit"
+
+#define STUDENT_MENU                                                           \
+	".......... Welcome to Student Menu ..........\n1. View All "          \
+	"Courses\n2. Enroll New Course\n3. Drop Course\n4. View Enrolled "     \
+	"Course Details\n5. Change Password\n6. Logout and Exit"
+
+#endif
